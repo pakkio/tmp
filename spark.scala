94987e9111e6913ad4057f2130859530eae8bae7
Claudio Pacchiega <pakkio@users.noreply.github.com>
2017-09-07 01:05:47 +0200

    Update spark.scala

--- spark.scala
+++ spark.scala
@@ -1,8 +1,18 @@
-// create organizations from a url
-val html = scala.io.Source.fromURL("https://raw.githubusercontent.com/pakkio/tmp/master/organizations.json").mkString
-val list = html.split("\n").filter(_ != "")
-val rddOrgJsonString = sc.parallelize(list)
+def getFileContentAsRDD(s: String) = {
+  val html = scala.io.Source.fromURL(s"https://raw.githubusercontent.com/pakkio/tmp/master/$s").mkString
+  val list = html.split("\n").filter(_ != "")
+  sc.parallelize(list)
+}
+// Get json from my github pakkio/tmp project
+def getGitJson(session: SparkSession, s: String) = {   
+  session.read.json(getFileContentAsRDD(s)
+}
+                   
+def getStringData(session: SparkSession, s: String) = {
+   session.read.text(getFileContentAsRDD(s))
+}
+
 val spark = SparkSession
    .builder().getOrCreate()
-val rddOrganizations = spark.read.json(rddOrgJsonString)
-rddOrganizations.show
+val orgs = getGitJson(spark,"organizations.json")
+val analyticsData = getStringData(spark,"analytics.json")
